1ec0d7e8fb8b4d6795ab4a5b9badcac2b5f4d675
Tim203 <mctim203@gmail.com>
2021-08-27 17:08:18 +0200

    Explain the use of the packet blocker

--- common/src/main/java/org/geysermc/floodgate/addon/data/PacketBlocker.java
+++ common/src/main/java/org/geysermc/floodgate/addon/data/PacketBlocker.java
@@ -30,6 +30,19 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import java.util.Queue;
 
+/**
+ * In Floodgate the PacketBlocker is used to temporarily prevent packets from being decoded. A
+ * similar system is used to prevent packets from being handled while Floodgate is processing the
+ * login. The old system blocked the thread which was processing the Floodgate login, but that
+ * doesn't only block the packets for that specific user, it's shared between multiple users causing
+ * them to lag or sometimes timeout.
+ * <br>
+ * The reason why we prevent packets from being handled is because keeping the packet order is
+ * important. That is also the reason why we prevent packets from being decoded during that time.
+ * The 'login start' packet for example can only be decoded after the handshake packet has been
+ * handled, because the server can only successfully decode the packet after the handshake packet
+ * caused the server to switch to the login state.
+ */
 public class PacketBlocker extends ChannelInboundHandlerAdapter {
     private final Queue<Object> packetQueue = Queues.newConcurrentLinkedQueue();
     private volatile boolean blockPackets;
