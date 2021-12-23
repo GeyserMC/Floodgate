5b5ec3d8ee45f31d7ca82ff8320f068b92e33751
Gadget <94780999+Gadget64@users.noreply.github.com>
2021-12-23 05:49:06 +0100

    Stop news checker on shutdown (#248)

--- common/src/main/java/org/geysermc/floodgate/FloodgatePlatform.java
+++ common/src/main/java/org/geysermc/floodgate/FloodgatePlatform.java
@@ -145,6 +145,7 @@ public class FloodgatePlatform {
             }
         }
 
+        guice.getInstance(NewsChecker.class).shutdown();
         api.getPlayerLink().stop();
         return true;
     }
