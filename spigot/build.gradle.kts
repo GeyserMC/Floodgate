d2afbc22cf8c4634d5064d93b63302b65e822c2c
thenilsdev <contact@ni.ls>
2022-07-26 19:48:55 +0200

    fix: Fix #322 deepCopy pre 1.17

--- spigot/build.gradle.kts
+++ spigot/build.gradle.kts
@@ -8,6 +8,8 @@ dependencies {
     implementation("cloud.commandframework", "cloud-bukkit", Versions.cloudVersion)
     // hack to make pre 1.12 work
     implementation("com.google.guava", "guava", guavaVersion)
+    // hack to make pre 1.17 work
+    implementation("com.google.code.gson", "gson", gsonVersion)
 }
 
 relocate("com.google.inject")
@@ -16,6 +18,7 @@ relocate("cloud.commandframework")
 relocate("io.leangen.geantyref") // used in cloud
 // hack to make pre 1.12 work
 relocate("com.google.common")
+relocate("com.google.gson")
 relocate("com.google.guava")
 // hack to make (old versions? of) Paper work
 relocate("it.unimi")
@@ -25,5 +28,4 @@ provided("com.destroystokyo.paper", "paper-api", Versions.spigotVersion)
 provided("com.mojang", "authlib", authlibVersion)
 provided("io.netty", "netty-transport", Versions.nettyVersion)
 provided("io.netty", "netty-codec", Versions.nettyVersion)
-provided("com.google.code.gson", "gson", gsonVersion)
 provided("org.yaml", "snakeyaml", Versions.snakeyamlVersion)
