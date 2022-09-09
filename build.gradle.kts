4fad984dace1425eb00d0c27417c9cccda87ad56
Robin <robin.braemer@web.de>
2022-09-09 15:50:14 +0200

    0.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 allprojects {
     group = "com.minekube.connect"
-    version = "0.2.0-SNAPSHOT"
+    version = "0.2.1"
     description =
         "Connects the server/proxy to the global Connect network to reach more players while also supporting online mode server, bungee or velocity mode. Visit https://minekube.com/connect"
 }
