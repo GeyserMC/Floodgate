5b9bd51ad12c52a5b16be37ea54e2a7f73d78b66
Konicai <71294714+Konicai@users.noreply.github.com>
2022-09-20 21:10:17 -0400

    Add remapped shadow jar, update gradle

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import net.fabricmc.loom.task.RemapJarTask
 
 val minecraftVersion = "1.19"
 val loaderVersion = "0.14.6"
@@ -17,11 +19,49 @@ java {
 
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
+    sourceCompatibility = "17"
+    targetCompatibility = "17"
     options.release.set(17)
 }
 
 loom {
     accessWidenerPath.set(file("src/main/resources/floodgate.accesswidener"))
+}
+
+// Loom will automatically attach sourcesJar to a RemapSourcesJar task and to the "build" task
+// if it is present.
+// If you remove this task, sources will not be generated.
+//val sourcesJar = tasks.create<Jar>("sourcesJar") {
+    //archiveClassifier.set("sources")
+    //from(sourceSets["main"].allSource)
+//}
+
+// This is easier than what is immediately above?
+java {
+    withSourcesJar()
+}
+
+// fixme: this should exist apparently
+//val shadowJar by tasks.getting(ShadowJar::class) {
+    //configurations = listOf(
+        //project.configurations.shadow.fileCollection()
+    //)
+//}
+
+val remappedShadowJar = tasks.create<RemapJarTask>("remappedShadowJar") {
+    dependsOn(tasks.shadowJar)
+    input.set(tasks.shadowJar.get().archiveFile) //fixme: deprecated
+    addNestedDependencies.set(true)
+    archiveFileName.set("floodgate-fabric.jar")
+}
+
+tasks.assemble {
+    dependsOn(remappedShadowJar)
+}
+
+artifacts {
+    archives(remappedShadowJar)
+    shadow(tasks.shadowJar)
 }
 
 repositories {
@@ -32,6 +72,7 @@ repositories {
     }
 }
 
+// todo: perform exclusions using floodgate build logic
 dependencies {
     api(projects.core) {
         exclude("com.google.guava", "guava")
@@ -44,14 +85,14 @@ dependencies {
     minecraft("com.mojang", "minecraft", minecraftVersion)
     mappings(loom.officialMojangMappings())
 
-    modImplementation("net.fabricmc:fabric-loader:${loaderVersion}")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:${fabricVersion}")
+    modImplementation("net.fabricmc", "fabric-loader", loaderVersion)
+    modImplementation("net.fabricmc.fabric-api", "fabric-api", fabricVersion)
 
-    include(modImplementation("cloud.commandframework:cloud-fabric:1.7.0-SNAPSHOT") {
+    include(modImplementation("cloud.commandframework", "cloud-fabric", "1.7.0-SNAPSHOT") {
         because("Commands library implementation for Fabric")
     })
 
-    include(modImplementation("net.kyori:adventure-platform-fabric:5.4.0-SNAPSHOT") {
+    include(modImplementation("net.kyori", "adventure-platform-fabric", "5.4.0-SNAPSHOT") {
         because("Chat library implementation for Fabric that includes methods for communicating with the server")
         // Thanks to zml for this fix
         // The package modifies Brigadier which causes a LinkageError at runtime if included
