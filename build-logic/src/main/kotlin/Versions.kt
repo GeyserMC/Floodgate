92c9a508dbd3e996fa70e1330b6002b2db152277
rtm516 <rtm516@users.noreply.github.com>
2024-01-28 22:37:26 +0000

    Update Geyser version

--- build-logic/src/main/kotlin/Versions.kt
+++ build-logic/src/main/kotlin/Versions.kt
@@ -24,7 +24,7 @@
  */
 
 object Versions {
-    const val geyserVersion = "2.1.2-SNAPSHOT"
+    const val geyserVersion = "2.2.0-SNAPSHOT"
     const val cumulusVersion = "1.1.2"
     const val eventsVersion = "1.1-SNAPSHOT"
     const val configUtilsVersion = "1.0-SNAPSHOT"
