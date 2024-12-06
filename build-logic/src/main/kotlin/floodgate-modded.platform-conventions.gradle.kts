6723ee7875991fdaf64d5f2a717a0c7123d69906
chris <github@onechris.mozmail.com>
2024-12-06 13:18:07 +0800

    Indicate 1.21.4 support on modrinth (#139)

--- build-logic/src/main/kotlin/floodgate-modded.platform-conventions.gradle.kts
+++ build-logic/src/main/kotlin/floodgate-modded.platform-conventions.gradle.kts
@@ -133,6 +133,6 @@ modrinth {
     syncBodyFrom.set(rootProject.file("README.md").readText())
 
     uploadFile.set(tasks.remapJar.get().destinationDirectory.get().asFile.resolve("${versionName(project)}.jar"))
-    gameVersions.add(libs.minecraft.get().version as String)
+    gameVersions.addAll(libs.minecraft.get().version as String, "1.21.4")
     failSilently.set(false)
 }
