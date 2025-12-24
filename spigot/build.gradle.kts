4d3e10bd46ddf995cdc11670937ba100dd95b72f
AnnotationVisitor <231545974+AnnotationVisitor@users.noreply.github.com>
2025-12-24 18:28:53 +0000

    Update build.gradle.kts

--- spigot/build.gradle.kts
+++ spigot/build.gradle.kts
@@ -5,8 +5,8 @@ var gsonVersion = "2.8.5"
 indra {
     javaVersions {
         // For Folia
-        target(8)
-        minimumToolchain(17)
+        target(21)
+        minimumToolchain(21)
     }
 }
 
@@ -19,7 +19,7 @@ dependencies {
 
     compileOnlyApi("dev.folia", "folia-api", Versions.spigotVersion) {
         attributes {
-            attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, 17)
+            attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, 21)
         }
     }
 }
