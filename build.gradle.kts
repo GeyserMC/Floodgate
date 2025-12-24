035cb2262609193ecef9a88236d3c4a4aa4c6fb4
AnnotationVisitor <231545974+AnnotationVisitor@users.noreply.github.com>
2025-12-24 18:28:37 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     `java-library`
     id("floodgate.build-logic")
-    id("io.freefair.lombok") version "6.3.0" apply false
+    id("io.freefair.lombok") version "8.6" apply false
 }
 
 allprojects {
@@ -10,14 +10,13 @@ allprojects {
     description = "Allows Bedrock players to join Java edition servers while keeping the server in online mode"
 }
 
-val deployProjects = setOf(
-    projects.api,
-    // for future Floodgate integration + Fabric
-    projects.core,
-    projects.bungee,
-    projects.spigot,
-    projects.velocity
-).map { it.dependencyProject }
+val deployProjects: Set<Project> = setOf(
+    project(":api"),
+    project(":core"),
+    project(":bungee"),
+    project(":spigot"),
+    project(":velocity")
+)
 
 //todo re-add checkstyle when we switch back to 2 space indention
 // and take a look again at spotbugs someday
@@ -36,8 +35,9 @@ subprojects {
         plugins.apply("floodgate.database-conventions")
     }
 
-    when (this) {
-        in deployProjects -> plugins.apply("floodgate.publish-conventions")
-        else -> plugins.apply("floodgate.base-conventions")
+    if (deployProjects.contains(project)) {
+        plugins.apply("floodgate.publish-conventions")
+    } else {
+        plugins.apply("floodgate.base-conventions")
     }
 }
